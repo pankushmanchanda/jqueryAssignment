5fbf65856521cc67c4e5d710906d81dc9d388f5d
pankushmanchanda <manchandapankush@gmail.com>
2017-04-21 01:08:30 +0530

    Wheather forecast

--- js/api.js
+++ js/api.js
@@ -1,50 +1,41 @@
 $(function() {
-  $('#btnSearch').on("click", function() {
-    apiCall();
-  });
-
-  function apiCall() {
-    $('#jsonTable').html('');
-    var title = $('#txtSearch').val();
-    $.getJSON('http://www.omdbapi.com/?s=' + title).then(function(response) {
-      var data = response
-      console.log(response);
-      var columns = addAllColumnHeaders(data);
-
-      for (var i = 0; i < data["Search"].length; i++) {
-        var row$ = $('<tr/>');
-        for (var colIndex = 0; colIndex < columns.length; colIndex++) {
-          var cellValue = data["Search"][i][columns[colIndex]];
-          if (colIndex == 4) {
-            if (cellValue == null) { cellValue = ""; }
-            row$.append($('<td><img src=' + data["Search"][i][columns[4]] + ' alt="Image" height="300px" width="100%"></td>'));
-          } else {
-            if (cellValue == null) { cellValue = ""; }
-            row$.append($('<td/>').html(cellValue));
-          }
-        }
-        $("#jsonTable").append(row$);
-      }
-
+    $('#btnSearch').on("click", function() {
+        apiCall();
     });
-  }
 
-  function addAllColumnHeaders(myList) {
-    var columnSet = [];
-    var headerTr$ = $('<tr/>');
+    function apiCall() {
+        debugger;
 
-    for (var i = 0; i < myList["Search"].length; i++) {
-      var rowHash = myList["Search"][i];
-      for (var key in rowHash) {
-        if ($.inArray(key, columnSet) == -1) {
-          columnSet.push(key);
-          headerTr$.append($('<th/>').html(key));
+        var title = $('#txtSearch').val();
+        $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=" + title + "&appid=f20e1627db6b44f68298e4681f048e59", function(data) {
+            console.log(data);
+            getData(data);
+        });
+    };
+
+    function getData(data) {
+        debugger;
+        $('#data').html(' ');
+        var iconCode = data.list[0].weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+        for (var i = 0; i < data.list.length; i = i + 8) {
+            $('#data').append('<div class="container-fluid well fixed" style="margin-right:30%; margin-left:30%; margin-top: 50px;"><p id="location">' + data.city.name + '</p><p id="weather"><b>Weather Status:</b> ' + data.list[i].weather[0].description + '<img src="' + iconUrl + '"></p><p id="temperature"><b>Temperature:</b> ' + Math.round(data.list[i].main.temp) + '&deg;' + '</p></div>');
         }
-      }
+        //     $('#temperature').html("<b>Temperature:</b> " + Math.round(data.list[0].main.temp) + '&deg;');
+        //     $('#location').html(data.city.name);
+        //     //button to convert C and F, it must have two functions
+
+
+        //     $("#weather").html("<b>Weather Status:</b> " + data.list[0].weather[0].description + "<img src='" + iconUrl + "'>");
+        // 
     }
-    $("#jsonTable").append(headerTr$);
+    // $("#F").click(function() {
+    //     $('#temperature').html("<b>Temperature:</b> " + (Math.round(data.list[0].main.temp) * (9 / 5) + 32) + '&deg;');
+    // });
 
-    return columnSet;
-  }
+    // $("#C").click(function() {
+    //     $('#temperature').html("<b>Temperature:</b> " + Math.round(data.main.temp) + '&deg;');
 
+    // });
 });
