5ca9bff1239bad0cee128a4a5eecf0a0b22d431d
pankushmanchanda <manchandapankush@gmail.com>
2017-04-20 12:36:22 +0530

    first jquery assignment

---
+++ js/api.js
@@ -0,0 +1,50 @@
+$(function() {
+  $('#btnSearch').on("click", function() {
+    apiCall();
+  });
+
+  function apiCall() {
+    $('#jsonTable').html('');
+    var title = $('#txtSearch').val();
+    $.getJSON('http://www.omdbapi.com/?s=' + title).then(function(response) {
+      var data = response
+      console.log(response);
+      var columns = addAllColumnHeaders(data);
+
+      for (var i = 0; i < data["Search"].length; i++) {
+        var row$ = $('<tr/>');
+        for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+          var cellValue = data["Search"][i][columns[colIndex]];
+          if (colIndex == 4) {
+            if (cellValue == null) { cellValue = ""; }
+            row$.append($('<td><img src=' + data["Search"][i][columns[4]] + ' alt="Image" height="300px" width="100%"></td>'));
+          } else {
+            if (cellValue == null) { cellValue = ""; }
+            row$.append($('<td/>').html(cellValue));
+          }
+        }
+        $("#jsonTable").append(row$);
+      }
+
+    });
+  }
+
+  function addAllColumnHeaders(myList) {
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
+
+    for (var i = 0; i < myList["Search"].length; i++) {
+      var rowHash = myList["Search"][i];
+      for (var key in rowHash) {
+        if ($.inArray(key, columnSet) == -1) {
+          columnSet.push(key);
+          headerTr$.append($('<th/>').html(key));
+        }
+      }
+    }
+    $("#jsonTable").append(headerTr$);
+
+    return columnSet;
+  }
+
+});
